5c0f2e1988e85c6be631f48c7338cee26c2dbcd9
FadyPhilips <fadyaphilips97@gmail.com>
2020-06-12 19:20:37 -0230

    created an IMDb Rating component which takes in a number

---
+++ src/UI/IMDbRating.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import IMDbIcon from "../IMDb-Icon.png";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  text-align: center;
+`;
+const StyledRating = styled.div`
+  font-size: 23px;
+  color: #c1c1c1;
+`;
+const StyledIcon = styled.img`
+  align-self: center;
+  margin: 5px;
+  margin-right: 15px;
+`;
+
+const IMDbRating = (props) => {
+  return (
+    <StyledDiv>
+      <StyledIcon src={IMDbIcon} alt="oops" width="auto" height="25px" />
+      <StyledRating>{props.rating.toFixed(1)}/10</StyledRating>
+    </StyledDiv>
+  );
+};
+
+export default IMDbRating;
