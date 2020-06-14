e8f2ae6bc767146ee3b6dd5543efacd70b7d8ada
FadyPhilips <fadyaphilips97@gmail.com>
2020-06-14 16:44:07 -0230

    Created User Icon It is a circle with a letter in it The background color of the circle is picked randomly from a list of hexadecimal colors The Letter is extracted from a string which is passed in as a prop called username

--- src/API/API.js
+++ src/API/API.js
@@ -1,49 +1,55 @@
-import axios from "axios"
-
-
+import axios from "axios";
 
 class API {
-    static signUp(email, password) {
-        return axios.post('http://localhost:5000/signUp', {
-            name: "youssef",
-            email,
-            password
-        }).then((response) => {
-            // handle success
-            console.log(response);
-            localStorage.setItem("token", response.data.token)
-        })
-    }
+  static signUp(email, password) {
+    return axios
+      .post("http://localhost:5000/signUp", {
+        name: "youssef",
+        email,
+        password,
+      })
+      .then((response) => {
+        // handle success
+        console.log(response);
+        localStorage.setItem("token", response.data.token);
+      });
+  }
 
-    static isLoggedIn() {
-        return axios.post("http://localhost:5000/profile", {
-            token: localStorage.getItem("token")
-        }).then((res) => {
-            return true
-        }).catch(() => {
-            return false
-        })
-    }
+  static isLoggedIn() {
+    return axios
+      .post("http://localhost:5000/profile", {
+        token: localStorage.getItem("token"),
+      })
+      .then((res) => {
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
+  }
 
-    static logout() {
-        return axios.post("http://localhost:5000/logout", {
-            token: localStorage.getItem("token")
-        }).then(() => {
-            localStorage.removeItem("token")
-            console.log("LoggedOut")
-        })
-    }
+  static logout() {
+    return axios
+      .post("http://localhost:5000/logout", {
+        token: localStorage.getItem("token"),
+      })
+      .then(() => {
+        localStorage.removeItem("token");
+        console.log("LoggedOut");
+      });
+  }
 
-    static login(email, password) {
-        return axios.post("http://localhost:5000/login", {
-            email,
-            password
-        }).then((res) => {
-            localStorage.setItem("token", res.data.token)
-            console.log("loggedIn")
-        })
-    }
+  static login(email, password) {
+    return axios
+      .post("http://localhost:5000/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        console.log("loggedIn");
+      });
+  }
 }
 
-export default API
-
+export default API;
---
+++ src/UI/UserIcon.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const colorList = [
+  "#597ef7",
+  "#49aa19",
+  "#13a8a8",
+  "#cb2b83",
+  "#642ab5",
+  "#177ddc",
+  "#8bbb11",
+  "#d8bd14",
+  "#d89614",
+  "#d32029",
+  "#d84a1b",
+  "#d87a16",
+];
+
+const randomColor =
+  colorList[Math.floor(Math.random() * Math.floor(colorList.length))];
+console.log(randomColor);
+
+const UserIconDiv = styled.div`
+  width: 40px;
+  margin: 10px;
+  border-radius: 50%;
+  background-color: ${randomColor};
+  text-align: center;
+  line-height: 40px;
+  margin: 10px;
+`;
+const StyledLetter = styled.div`
+  font-size: 25px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+const UserIcon = (props) => {
+  return (
+    <UserIconDiv>
+      <StyledLetter>{props.username.charAt()}</StyledLetter>
+    </UserIconDiv>
+  );
+};
+
+export default UserIcon;
