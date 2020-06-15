4ae2d66b4d78588f4b5a7c20ceb5fd5a227c05b4
FadyPhilips <fadyaphilips97@gmail.com>
2020-06-14 21:31:41 -0230

    Created a search bar that takes a function for onChange It also takes in a prop for background color

---
+++ src/UI/SearchBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { SearchOutlined } from "@ant-design/icons";
+
+const searchIconStyling = {
+  color: "#c1c1c1",
+  fontSize: "14px",
+  marginRight: "8px",
+};
+
+const StyledInput = styled.input`
+  background-color: ${(props) =>
+    props.border === undefined ? "rgba(0,0,0,0)" : props.backgroundColor};
+  color: #c1c1c1;
+  width: 200px;
+  border: none;
+  outline: none;
+  font-size: 14px;
+`;
+
+const OuterDiv = styled.div`
+  background-color: ${(props) =>
+    props.backgroundColor === undefined
+      ? "rgba(0,0,0,0)"
+      : props.backgroundColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  border-radius: 8px;
+  width: 255px;
+  height: fit-content;
+  margin: 10px;
+`;
+
+const SearchBar = (props) => {
+  return (
+    <OuterDiv backgroundColor={props.backgroundColor}>
+      <SearchOutlined style={searchIconStyling} />
+      <StyledInput
+        backgroundColor={props.backgroundColor}
+        placeholder="Search"
+        type="text"
+        onChange={props.onInputChange}
+      />
+    </OuterDiv>
+  );
+};
+
+export default SearchBar;
