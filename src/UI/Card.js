30d43ecec8048a7a1e1a4718eb08a51d0b833db6
amr mohamed <arsmohamed@outlook.com>
2020-06-17 15:32:37 -0230

    finished havor but not stable

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -12,8 +12,15 @@ const StyledCard = styled.div`
     background-image:url("https://m.media-amazon.com/images/M/MV5BODY1NWE2OTctOTU5MC00NTlmLWI2MzktMmYzMTUzYjk4YjEzXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_SY1000_CR0,0,701,1000_AL_.jpg");
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    
+    &:hover {
+        
+    }
 `;
+const HoverDiv = styled.div`
+background-color : rgba(0,0,0,0.5);
+height: 100%;
+width:100%;
+`
 const IconPosition = {
     position: "relative",
     top: "20%",
@@ -36,14 +43,20 @@ const IMDB = styled.div`
     width: 50%;
     background-image:url("https://images6.alphacoders.com/612/612531.jpg");
 `;
+
+
 const AppCard = (props) => {
     const [inVal, exVal] = useState(true);
     const EditValue = (v) => {
         v.inVal !== inVal && exVal(!inVal);
-    }
+    };
+    let [inOnHover, exOnHover] = useState(true);
+    const Hovering = (val) => {
+        val.inOnHover !== inOnHover && exOnHover(!inOnHover);
+    };
     const isWork = () => {
         return (
-            <div onClick={EditValue} styled="transition:0s 180s">
+            <div onClick={EditValue}>
                 {
                     inVal === true ?
                         <PlusCircleFilled style={IconPosition} addToList={props.isInList} />
@@ -56,17 +69,34 @@ const AppCard = (props) => {
             </div>
         )
     }
+    const OnHover = () => {
+        if (inOnHover) {
+            return (
+                <HoverDiv>
+                    <div style={Info}> <InfoCircleFilled /></div>
+                    <div style={IconHeight}>
+                        {isWork()}
+                    </div>
+                    <div > <IMDB /> IMDB</div>
+                </HoverDiv>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
 
     return (
-        <StyledCard>
-            <div style={Info}> <InfoCircleFilled /></div>
-            <div style={IconHeight}>
-                {isWork()}
-            </div>
-            <div > <IMDB /> IMDB</div>
+        <StyledCard onMouseOver={Hovering} onmouseleave={Hovering}>
+            {OnHover()}
+
             <div >Title {props.title} </div>
         </StyledCard>
+
     )
+
 };
 
 export default AppCard;
