c1c65645d21439d8253b33657d2ac26376d48ee7
Mero <mero@Meros-iMac-Pro.local>
2020-06-19 07:04:57 -0230

    ready for pull req

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
           <Route path="/sign-up" component={SignUp} />
         </Switch>
       </div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card BGURL={"https://m.media-amazon.com/images/M/MV5BZjNlZmUyYmMtNjNjMS00NzQ5LTlmYjktNDVkMmRjMTQyMmVjXkEyXkFqcGdeQXVyNzk0NTA5NQ@@._V1_SY1000_CR0,0,677,1000_AL_.jpg"} />
+      <Card BGURL={"https://m.media-amazon.com/images/M/MV5BODY1NWE2OTctOTU5MC00NTlmLWI2MzktMmYzMTUzYjk4YjEzXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_SY1000_CR0,0,701,1000_AL_.jpg"} />
+      <Card BGURL={"https://m.media-amazon.com/images/M/MV5BYmI3NDRlYzUtZWRkMC00ZjU2LWFkYjAtMzYzY2UyNGYxOWJmXkEyXkFqcGdeQXVyMzI2NzE3MDc@._V1_.jpg"} />
+      <Card BGURL={"https://m.media-amazon.com/images/M/MV5BMWRmNDRkN2ItYTEyNS00NWVhLWFkNzgtNWJkM2RkOTcyNWRmXkEyXkFqcGdeQXVyMzE4MDkyNTA@._V1_.jpg"} />
+      <Card BGURL={"https://m.media-amazon.com/images/M/MV5BY2Y4MTE2MDEtOTRhNS00Yzk0LTk0M2MtNzJmMGI5YmIxNmE1XkEyXkFqcGdeQXVyMzE4MDkyNTA@._V1_.jpg"} />
 
     </Router>
   );
--- src/UI/Card.js
+++ src/UI/Card.js
@@ -10,24 +10,33 @@ const StyledCard = styled.div`
     box-shadow: 0 2px 5px #ccc;
     margin: 15px 15px 15px 15px;
     float: left;
-    background-image:url("https://m.media-amazon.com/images/M/MV5BZjNlZmUyYmMtNjNjMS00NzQ5LTlmYjktNDVkMmRjMTQyMmVjXkEyXkFqcGdeQXVyNzk0NTA5NQ@@._V1_SY1000_CR0,0,677,1000_AL_.jpg");
+    background-image:url(${ (props) => props.BackGroundURL});
     background-repeat: no-repeat;
     background-size: 100% 100%;
-    &:hover {
-        
-    }
 `;
+const EmptyDiv = {
+    height: "90%",
+    width: "100%",
+    borderRadius: "7px",
+}
 const HoverDiv = styled.div`
-    background-color : rgba(0,0,0,0.5);
-    opacity:${(props) => props.opacity};
+    background-color : rgba(0,0,0,0.5); 
     height: 100%;
     width:100%;
     border-radius: 7px;
     box-shadow: 0 2px 5px #ccc;
-`
+    `
+const Info = {
+    position: "relative",
+    fontSize: "30px",
+    left: "80%",
+    top: "2%",
+    height: "30%",
+    color: "white",
+};
 const IconAddPosition = {
     position: "relative",
-    top: "15%",
+    top: "20%",
     left: "38%",
     padding: "0",
     fontSize: "60px",
@@ -43,26 +52,10 @@ const IconPosition = {
     color: "white",
     marginTop: "10px",
 };
-const Info = {
-    position: "relative",
-    fontSize: "30px",
-    left: "80%",
-    top: "2%",
-    height: "30%",
-    color: "white",
-};
-const IconHeight = {
-    height: "60%",
-};
 const IMDBStyle = {
     top: '80%',
     textAlignItems: 'center',
     backgroundColor: "rgba(0,0,0,0.7)",
-}
-const EmptyDiv = {
-    height: "90%",
-    width: "100%",
-    borderRadius: "7px",
 }
 const IMDBPostion = {
     margin: "1px 1px 1px 30px",
@@ -74,36 +67,21 @@ const AppCard = (props) => {
         v.inVal !== inVal && exVal(!inVal);
     };
     let [isHovering, setIsHovering] = useState(false);
-    const [overLayOpacity, setOverlayOpacity] = useState(0)
     const Hovering = (val) => {
         val.isHovering !== isHovering && setIsHovering(!isHovering);
     };
-
-    // const onHover = (val) => {
-    //     setIsHovering(true);
-    //     setInterval(() => {
-    //         console.log("object")
-    //         setOverlayOpacity(overLayOpacity + 0.6)
-    //     }, 200);
-
-    // }
-    // const onExitHover = (val) => {
-    //     setIsHovering(false);
-    //     setOverlayOpacity(0);
-    // }
-
     const shouldShowOverlay = () => {
         if (isHovering) {
             return (
                 <HoverDiv >
                     <div style={Info}> <InfoCircleFilled /></div>
-                    <div style={IconHeight}>
+                    <div >
                         <div onClick={EditValue}>
                             {
                                 inVal === true ?
-                                    <PlusCircleFilled style={IconAddPosition} addToList={props.isInList} />
+                                    <div style={IconAddPosition}><PlusCircleFilled addToList={props.addToList} /></div>
                                     :
-                                    <div>
+                                    <div >
                                         <div><CheckCircleFilled style={IconPosition} isInList={props.isInList} /></div>
                                         <div><MinusCircleFilled style={IconPosition} removeFromList={props.removeFromList} /></div>
                                     </div>
@@ -117,11 +95,11 @@ const AppCard = (props) => {
     }
 
     return (
-        <StyledCard onPointerEnter={Hovering} onMouseLeave={Hovering} opacity={overLayOpacity}>
+        <StyledCard onPointerEnter={Hovering} onMouseLeave={Hovering} BackGroundURL={props.BGURL}>
             <div style={EmptyDiv} > {shouldShowOverlay()}</div>
-            <div style={IMDBStyle} >  <div style={IMDBPostion}><IMDB rating={4} /></div></div>
-            <div >Title is  {props.title} </div>
-        </StyledCard>
+            <div style={IMDBStyle} >  <div style={IMDBPostion}> <IMDB rating={7.8} /></div></div>
+            <div >Title {props.title} </div>
+        </StyledCard >
 
     )
 
