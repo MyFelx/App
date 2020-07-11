0e492ed1442fcfc56931cd00ab5b8fed2ebaa5d3
FadyPhilips <fadyaphilips97@gmail.com>
2020-07-11 14:12:25 -0230

    Created Filter Group component and Movie Filters component
    FilteraGroup is a generic set of checkboxes
    MovieFilters is a parent of FilterGroup

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,8 +1,26 @@
-import React from "react";
-import MovieCard from "../UI/MovieCard"
+import React, { Component } from "react";
+import MovieFliters from "../UI/Fliters/MovieFliters";
 
-const Home = (props) => {
-  return <MovieCard movieRating={3} posterSrc={"https://images.moviepostershop.com/dora-and-the-lost-city-of-gold-movie-poster-1000779403.jpg"} title="dwadwd" isInList={false} />;
-};
+class Home extends Component {
+  state = {
+    activeFilters: {},
+  };
+
+  onFilterUpdate = (newActiveFilters) => {
+    this.setState({ activeFilters: newActiveFilters }, () => {
+      console.log(this.state.activeFilters);
+    });
+  };
+
+  render() {
+    return (
+      <MovieFliters
+        onUpdateFilter={(newActiveFilters) =>
+          this.onFilterUpdate(newActiveFilters)
+        }
+      />
+    );
+  }
+}
 
 export default Home;
